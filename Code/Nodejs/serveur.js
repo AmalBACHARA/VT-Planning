3e2d1a03e1ff4e35728bb08e127063037c5cd99b
DiarraDIOP <sokhna.d.diop@gmail.com>
2015-10-27 16:05:19 +0100

    Répertoire de tests pour le serveur Nodejs

---
+++ Code/Nodejs/serveur.js
@@ -0,0 +1,21 @@
+var http = require('http');
+var url = require('url');
+
+var server = http.createServer(function(req, res) {
+	//recuperer les paramètres de l'url
+    var page = url.parse(req.url).pathname;
+    console.log(page);
+    res.writeHead(200, {"Content-Type": "text/plain"});
+    if (page == '/') {
+        res.write('Vous êtes à l\'accueil, que puis-je pour vous ?');
+    }
+    else if (page == '/sous-sol') {
+        res.write('Vous êtes dans la cave à vins, ces bouteilles sont à moi !');
+    }
+    else if (page == '/etage/1/chambre') {
+        res.write('Hé ho, c\'est privé ici !');
+    }
+    res.end();
+});
+
+server.listen(8080);
