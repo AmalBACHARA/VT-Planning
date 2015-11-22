b9f86862837ac52b74c2630846d651ae82a6a897
DiarraDIOP <sokhna.d.diop@gmail.com>
2015-11-22 15:58:42 +0100

    fonctions de test

--- Code/Nodejs/js.js
+++ Code/Nodejs/js.js
@@ -14,20 +14,20 @@ $.ajax({url: "/matiere", success: function(result){ // putting "admin" need to b
  	 // alert(result.toJSON());
 //console.log(result.fulfillmentValue[0].nom);
               for (i = 0; i< result.fulfillmentValue.length; i++){
-           document.getElementById('listematiere').innerHTML += "<p>" + result.fulfillmentValue[i].nom+"</p>";
+           document.getElementById('listematiere').innerHTML += result.fulfillmentValue[j].nom ;
               }
          
         }});	
-
 
 $.ajax({url: "/filiere", success: function(result){ // putting "admin" need to be the pseudo log
  	// document.cookie ="username="+result[0].userName;
  	 // alert(result.toJSON());
 //console.log(result.fulfillmentValue[0].nom);
               for (j = 0; j< result.fulfillmentValue.length; j++){
-           document.getElementById('listematiere').innerHTML += "<p>" + result.fulfillmentValue[i].nom+"</p>";
+           document.getElementById('listematiere').innerHTML += result.fulfillmentValue[j].nom ;
               }
          
         }});		
 		
 });
+
